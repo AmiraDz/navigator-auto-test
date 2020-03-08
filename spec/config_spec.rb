6158c710528abc3821428c6fd77e05f8a07a595e
AmiraDz <adzanovic11@gmail.com>
2020-03-08 15:53:59 +0100

    On branch review  Initial commit  Changes to be committed: 	new file:   .gitignore 	new file:   Gemfile 	new file:   readme.md 	new file:   spec/config_spec.rb 	new file:   spec/regression_spec.rb 	new file:   spec/smoke_spec.rb

---
+++ spec/config_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require 'capybara'
+require 'selenium-webdriver'
+
+RSpec.configure do |config|
+  config.before(:all) do
+    Capybara::Session.new :selenium_chrome
+    current_window.maximize
+  end
+
+  config.before(:each) do
+    visit 'http://navigator.ba'
+  end
+end
+
+Capybara.configure do |config|
+  config.default_driver = :selenium_chrome
+  config.ignore_hidden_elements = false
+  config.default_max_wait_time = 5
+end
---
+++ spec/smoke_spec.rb
@@ -0,0 +1,33 @@
+valSearch = 'Atlantbh'
+expAddress = 'Milana Preloga 12A'
+
+name = 'House of Warriors'
+city = 'Sarajevo'
+zip = '71000'
+address = 'Terezija'
+houseNo = 'bb'
+mob = '062776867'
+
+describe 'smoke' do
+  it 'should search for valid term' do
+    fill_in id:'ember564', with: valSearch
+    find(class:'iconav-search').click
+    expect(page).to have_content(expAddress)
+  end
+
+  it 'should correctly fill out and submit form' do
+    find(id:'ember572').click
+    within('.expanded .nav-lefthand-form-container') do
+      fill_in 'poi_name', with: name
+      fill_in 'poi_city_name', with: city
+      fill_in 'poi_zip_code', with: zip
+      fill_in 'poi_place_id', with: address
+      fill_in 'poi_house_number', with: houseNo
+      click_button(text:'Odaberite kategoriju')
+      find('.category-selector-view .row select').first(:option,'Sport').select_option
+      fill_in 'poi_mobile_phone', with: mob
+      find_button(text:'Kreiraj').click
+    end
+    expect(page).to have_button(text:'Predloži izmjene')
+  end
+end
